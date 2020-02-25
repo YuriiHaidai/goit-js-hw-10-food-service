6a5b0f3ce7a804a312d441650970acb54d8bd18e
Gaiday_u <ura.kiev@gmail.com>
2020-02-25 11:06:38 +0200

    fix handler

--- src/index.js
+++ src/index.js
@@ -28,14 +28,16 @@ if (persistedTheme === Theme.DARK) {
   refs.themesSelector.checked = true;
 };
 
-refs.themesSelector.addEventListener('change', () => {
-if (refs.themesSelector.checked) {
-  refs.body.classList.add(Theme.DARK);
-  refs.body.classList.remove(Theme.LIGHT);
-  localStorage.setItem('theme', Theme.DARK);
-} else {
-  refs.body.classList.add(Theme.LIGHT);
-  refs.body.classList.remove(Theme.DARK);
-  localStorage.setItem('theme', Theme.LIGHT);
+refs.themesSelector.addEventListener('change', handleChange)
+
+function handleChange() {
+  if (refs.themesSelector.checked) {
+    refs.body.classList.add(Theme.DARK);
+    refs.body.classList.remove(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    refs.body.classList.add(Theme.LIGHT);
+    refs.body.classList.remove(Theme.DARK);
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
 }
-});
